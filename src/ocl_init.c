840c8a80ad8ef61ada3917021bdf81b5587875db
Atharva <atharva1@hokietest20.synergy>
2019-09-27 16:19:15 -0400

    add comments using //

--- src/ocl_init.c
+++ src/ocl_init.c
@@ -20,64 +20,64 @@ void init_ocl(struct context * context, const bool multigpu, const int rank)
     cl_int err;
 
     // Get list of platforms
-    /*
-    cl_uint num_platforms;
-    err = clGetPlatformIDs(0, NULL, &num_platforms);
-    check_ocl(err, "Getting number of platforms");
-    cl_platform_id *platforms = malloc(num_platforms*sizeof(cl_platform_id));
-    err = clGetPlatformIDs(num_platforms, platforms, &num_platforms);
-    check_ocl(err, "Getting platforms");
     
-    // Get a GPU device
-    cl_device_type type = CL_DEVICE_TYPE_GPU;
-    cl_uint num_devices = 0;
-    cl_device_id devices[MAX_DEVICES];
-    for (unsigned int i = 0; i < num_platforms; i++)
-    {
-        clGetDeviceIDs(platforms[i], type, MAX_DEVICES, devices, &num_devices);
-        if (num_devices == 1)
-            break;
-    }
-    free(platforms);
-    if (num_devices == 0)
-        check_ocl(CL_DEVICE_NOT_FOUND, "Cannot find a GPU device");
-
-
-    if (!multigpu)
-    {
-        // Just pick the first GPU device
-        context->device = devices[0];
-    #ifdef __APPLE__
-        // If we on my MacBook we need the second GPU (the discrete one)
-        context->device = devices[1];
-    #endif
-    }
-    else
-    {
-        // NOTE THIS ONLY WORKS FOR CRAY CS-STORM NODES
-        context->device = devices[rank];
-    }
-
-    // Create a context and command queue for the device
-    cl_platform_id *fplatforms;
-    //context->context = clCreateContext(0, 1, &context->device, NULL, NULL, &err);
-    check_ocl(err, "Creating context");
-  
-    if (profiling)
-    {
-        context->queue = clCreateCommandQueue(context->context, context->device, CL_QUEUE_PROFILING_ENABLE, &err);
-        check_ocl(err, "Creating command queue");
-        context->copy_queue = clCreateCommandQueue(context->context, context->device, CL_QUEUE_PROFILING_ENABLE, &err);
-        check_ocl(err, "Creating copy command queue");
-    }
-    else
-    {
-        context->queue = clCreateCommandQueue(context->context, context->device, 0, &err);
-        check_ocl(err, "Creating command queue");
-        context->copy_queue = clCreateCommandQueue(context->context, context->device, 0, &err);
-        check_ocl(err, "Creating copy command queue");
-    }
-    */
+//    cl_uint num_platforms;
+//    err = clGetPlatformIDs(0, NULL, &num_platforms);
+//    check_ocl(err, "Getting number of platforms");
+//    cl_platform_id *platforms = malloc(num_platforms*sizeof(cl_platform_id));
+//    err = clGetPlatformIDs(num_platforms, platforms, &num_platforms);
+//    check_ocl(err, "Getting platforms");
+//    
+//    // Get a GPU device
+//    cl_device_type type = CL_DEVICE_TYPE_GPU;
+//    cl_uint num_devices = 0;
+//    cl_device_id devices[MAX_DEVICES];
+//    for (unsigned int i = 0; i < num_platforms; i++)
+//    {
+//        clGetDeviceIDs(platforms[i], type, MAX_DEVICES, devices, &num_devices);
+//        if (num_devices == 1)
+//            break;
+//    }
+//    free(platforms);
+//    if (num_devices == 0)
+//        check_ocl(CL_DEVICE_NOT_FOUND, "Cannot find a GPU device");
+//
+//
+//    if (!multigpu)
+//    {
+//        // Just pick the first GPU device
+//        context->device = devices[0];
+//    #ifdef __APPLE__
+//        // If we on my MacBook we need the second GPU (the discrete one)
+//        context->device = devices[1];
+//    #endif
+//    }
+//    else
+//    {
+//        // NOTE THIS ONLY WORKS FOR CRAY CS-STORM NODES
+//        context->device = devices[rank];
+//    }
+//
+//    // Create a context and command queue for the device
+//    cl_platform_id *fplatforms;
+//    //context->context = clCreateContext(0, 1, &context->device, NULL, NULL, &err);
+//    check_ocl(err, "Creating context");
+//  
+//    if (profiling)
+//    {
+//        context->queue = clCreateCommandQueue(context->context, context->device, CL_QUEUE_PROFILING_ENABLE, &err);
+//        check_ocl(err, "Creating command queue");
+//        context->copy_queue = clCreateCommandQueue(context->context, context->device, CL_QUEUE_PROFILING_ENABLE, &err);
+//        check_ocl(err, "Creating copy command queue");
+//    }
+//    else
+//    {
+//        context->queue = clCreateCommandQueue(context->context, context->device, 0, &err);
+//        check_ocl(err, "Creating command queue");
+//        context->copy_queue = clCreateCommandQueue(context->context, context->device, 0, &err);
+//        check_ocl(err, "Creating copy command queue");
+//    }
+    
      cl_platform_id *fplatforms;
     meta_set_acc(1, metaModePreferOpenCL); //Must be set to OpenCL, don't need a device since we will override
     meta_get_state_OpenCL(&fplatforms ,&context->device, &context->context, &context->queue);
@@ -101,62 +101,62 @@ void init_ocl(struct context * context, const bool multigpu, const int rank)
     __meta_gen_opencl_sweep_plane_custom_args =options;
     __meta_gen_opencl_zero_buffer_custom_args  =options;
      meta_register_module(&meta_gen_opencl_metacl_module_registry);
-    /*
-    cl_int build_err = clBuildProgram(context->program, 1, &context->device, options, NULL, NULL);
-    if (build_err == CL_BUILD_PROGRAM_FAILURE)
-    {
-        size_t log_size;
-        err = clGetProgramBuildInfo(context->program, context->device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-        check_ocl(err, "Getting build log size");
-        char *build_log = malloc(log_size);
-        err = clGetProgramBuildInfo(context->program, context->device, CL_PROGRAM_BUILD_LOG, log_size, build_log, NULL);
-        check_ocl(err, "Getting build log");
-        fprintf(stderr, "OpenCL Build log: %s\n", build_log);
-        free(build_log);
-    }
-    check_ocl(build_err, "Building program");
-
-    // Create the kernels
-    context->kernels.calc_velocity_delta = clCreateKernel(context->program, "calc_velocity_delta", &err);
-    check_ocl(err, "Creating velocity delta kernel");
-    context->kernels.calc_dd_coeff = clCreateKernel(context->program, "calc_dd_coeff", &err);
-    check_ocl(err, "Creating diamond difference constants kernel");
-    context->kernels.calc_denominator = clCreateKernel(context->program, "calc_denominator", &err);
-    check_ocl(err, "Creating denominator kernel");
-    context->kernels.zero_buffer = clCreateKernel(context->program, "zero_buffer", &err);
-    check_ocl(err, "Creating buffer zeroing kernel");
-    context->kernels.outer_source = clCreateKernel(context->program, "calc_outer_source", &err);
-    check_ocl(err, "Creating outer source kernel");
-    context->kernels.inner_source = clCreateKernel(context->program, "calc_inner_source", &err);
-    check_ocl(err, "Creating inner source kernel");
-    context->kernels.sweep_plane = clCreateKernel(context->program, "sweep_plane", &err);
-    check_ocl(err, "Creating sweep plane kernel");
-    context->kernels.reduce_flux = clCreateKernel(context->program, "reduce_flux", &err);
-    check_ocl(err, "Creating scalar flux reduction kernel");
-    context->kernels.reduce_flux_moments = clCreateKernel(context->program, "reduce_flux_moments", &err);
-    check_ocl(err, "Creating scalar flux moments reduction kernel");
-   */
+    
+//    cl_int build_err = clBuildProgram(context->program, 1, &context->device, options, NULL, NULL);
+//    if (build_err == CL_BUILD_PROGRAM_FAILURE)
+//    {
+//        size_t log_size;
+//        err = clGetProgramBuildInfo(context->program, context->device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+//        check_ocl(err, "Getting build log size");
+//        char *build_log = malloc(log_size);
+//        err = clGetProgramBuildInfo(context->program, context->device, CL_PROGRAM_BUILD_LOG, log_size, build_log, NULL);
+//        check_ocl(err, "Getting build log");
+//        fprintf(stderr, "OpenCL Build log: %s\n", build_log);
+//        free(build_log);
+//    }
+//    check_ocl(build_err, "Building program");
+//
+//    // Create the kernels
+//    context->kernels.calc_velocity_delta = clCreateKernel(context->program, "calc_velocity_delta", &err);
+//    check_ocl(err, "Creating velocity delta kernel");
+//    context->kernels.calc_dd_coeff = clCreateKernel(context->program, "calc_dd_coeff", &err);
+//    check_ocl(err, "Creating diamond difference constants kernel");
+//    context->kernels.calc_denominator = clCreateKernel(context->program, "calc_denominator", &err);
+//    check_ocl(err, "Creating denominator kernel");
+//    context->kernels.zero_buffer = clCreateKernel(context->program, "zero_buffer", &err);
+//    check_ocl(err, "Creating buffer zeroing kernel");
+//    context->kernels.outer_source = clCreateKernel(context->program, "calc_outer_source", &err);
+//    check_ocl(err, "Creating outer source kernel");
+//    context->kernels.inner_source = clCreateKernel(context->program, "calc_inner_source", &err);
+//    check_ocl(err, "Creating inner source kernel");
+//    context->kernels.sweep_plane = clCreateKernel(context->program, "sweep_plane", &err);
+//    check_ocl(err, "Creating sweep plane kernel");
+//    context->kernels.reduce_flux = clCreateKernel(context->program, "reduce_flux", &err);
+//    check_ocl(err, "Creating scalar flux reduction kernel");
+//    context->kernels.reduce_flux_moments = clCreateKernel(context->program, "reduce_flux_moments", &err);
+//    check_ocl(err, "Creating scalar flux moments reduction kernel");
+   
 
 }
 
 void release_context(struct context * context)
 {
-    /*
-    cl_int err;
-    err = clReleaseProgram(context->program);
-    check_ocl(err, "Releasing program");
-
-#ifdef CL_VERSION_1_2
-    err = clReleaseDevice(context->device);
-    check_ocl(err, "Releasing device");
-#endif
-
-    err = clReleaseCommandQueue(context->queue);
-    check_ocl(err, "Releasing command queue");
-
-    err = clReleaseContext(context->context);
-    check_ocl(err, "Releasing context");
-    */
+    
+//    cl_int err;
+//    err = clReleaseProgram(context->program);
+//    check_ocl(err, "Releasing program");
+//
+//#ifdef CL_VERSION_1_2
+//    err = clReleaseDevice(context->device);
+//    check_ocl(err, "Releasing device");
+//#endif
+//
+//    err = clReleaseCommandQueue(context->queue);
+//    check_ocl(err, "Releasing command queue");
+//
+//    err = clReleaseContext(context->context);
+//    check_ocl(err, "Releasing context");
+    
     meta_deregister_module(&meta_gen_opencl_metacl_module_registry);
 
 }
--- src/problem.c
+++ src/problem.c
@@ -261,18 +261,18 @@ void init_velocity_delta(
 {
     // We do this on the device because SNAP does it every outer
     cl_int err;
-    /*
-    err = clSetKernelArg(context->kernels.calc_velocity_delta, 0, sizeof(cl_mem), &buffers->velocities);
-    err |= clSetKernelArg(context->kernels.calc_velocity_delta, 1, sizeof(double), &problem->dt);
-    err |= clSetKernelArg(context->kernels.calc_velocity_delta, 2, sizeof(cl_mem), &buffers->velocity_delta);
-    check_ocl(err, "Setting velocity delta calculation kernel arguments");
     
-    size_t global[] = {problem->ng};
-    err = clEnqueueNDRangeKernel(context->queue,
-        context->kernels.calc_velocity_delta,
-        1, 0, global, NULL,
-        0, NULL, &velocity_delta_event);
-    */
+//    err = clSetKernelArg(context->kernels.calc_velocity_delta, 0, sizeof(cl_mem), &buffers->velocities);
+//    err |= clSetKernelArg(context->kernels.calc_velocity_delta, 1, sizeof(double), &problem->dt);
+//    err |= clSetKernelArg(context->kernels.calc_velocity_delta, 2, sizeof(cl_mem), &buffers->velocity_delta);
+//    check_ocl(err, "Setting velocity delta calculation kernel arguments");
+//    
+//    size_t global[] = {problem->ng};
+//    err = clEnqueueNDRangeKernel(context->queue,
+//        context->kernels.calc_velocity_delta,
+//        1, 0, global, NULL,
+//        0, NULL, &velocity_delta_event);
+    
     size_t global[3] = {problem->ng,1,1};
     size_t local[3] = {0,0,0};
     err = meta_gen_opencl_calc_velocity_delta_calc_velocity_delta(context->queue, global, local, &buffers->velocities, problem->dt, &buffers->velocity_delta, 0, &velocity_delta_event);
@@ -288,23 +288,23 @@ void calculate_dd_coefficients(
 {
     // We do this on the device because SNAP does it every outer
     cl_int err;
-    /*
-    err = clSetKernelArg(context->kernels.calc_dd_coeff, 0, sizeof(double), &problem->dx);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 1, sizeof(double), &problem->dy);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 2, sizeof(double), &problem->dz);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 3, sizeof(cl_mem), &buffers->eta);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 4, sizeof(cl_mem), &buffers->xi);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 5, sizeof(cl_mem), &buffers->dd_i);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 6, sizeof(cl_mem), &buffers->dd_j);
-    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 7, sizeof(cl_mem), &buffers->dd_k);
-    check_ocl(err, "Setting diamond difference calculation kernel arguments");
-
-    size_t global[] = {problem->nang};
-    err = clEnqueueNDRangeKernel(context->queue,
-        context->kernels.calc_dd_coeff,
-        1, 0, global, NULL,
-        0, NULL, NULL);
-    */
+    
+//    err = clSetKernelArg(context->kernels.calc_dd_coeff, 0, sizeof(double), &problem->dx);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 1, sizeof(double), &problem->dy);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 2, sizeof(double), &problem->dz);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 3, sizeof(cl_mem), &buffers->eta);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 4, sizeof(cl_mem), &buffers->xi);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 5, sizeof(cl_mem), &buffers->dd_i);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 6, sizeof(cl_mem), &buffers->dd_j);
+//    err |= clSetKernelArg(context->kernels.calc_dd_coeff, 7, sizeof(cl_mem), &buffers->dd_k);
+//    check_ocl(err, "Setting diamond difference calculation kernel arguments");
+//
+//    size_t global[] = {problem->nang};
+//    err = clEnqueueNDRangeKernel(context->queue,
+//        context->kernels.calc_dd_coeff,
+//        1, 0, global, NULL,
+//        0, NULL, NULL);
+    
     size_t global[3] = {problem->nang,1,1};
     size_t local[3] = {0,0,0};
     err= meta_gen_opencl_calc_dd_coeff_calc_dd_coeff(context->queue, global, local, problem->dx, problem->dy, problem->dz, &buffers->eta, &buffers->xi, &buffers->dd_i, &buffers->dd_j, &buffers->dd_k, 0, NULL);
@@ -320,28 +320,28 @@ void calculate_denominator(
 {
     // We do this on the device because SNAP does it every outer
     cl_int err;
-    /*
-    err = clSetKernelArg(context->kernels.calc_denominator, 0, sizeof(unsigned int), &rankinfo->nx);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 1, sizeof(unsigned int), &rankinfo->ny);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 2, sizeof(unsigned int), &rankinfo->nz);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 3, sizeof(unsigned int), &problem->nang);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 4, sizeof(unsigned int), &problem->ng);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 5, sizeof(cl_mem), &buffers->mat_cross_section);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 6, sizeof(cl_mem), &buffers->velocity_delta);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 7, sizeof(cl_mem), &buffers->mu);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 8, sizeof(cl_mem), &buffers->dd_i);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 9, sizeof(cl_mem), &buffers->dd_j);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 10, sizeof(cl_mem), &buffers->dd_k);
-    err |= clSetKernelArg(context->kernels.calc_denominator, 11, sizeof(cl_mem), &buffers->denominator);
-    check_ocl(err, "Setting denominator kernel arguments");
+    
+//    err = clSetKernelArg(context->kernels.calc_denominator, 0, sizeof(unsigned int), &rankinfo->nx);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 1, sizeof(unsigned int), &rankinfo->ny);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 2, sizeof(unsigned int), &rankinfo->nz);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 3, sizeof(unsigned int), &problem->nang);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 4, sizeof(unsigned int), &problem->ng);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 5, sizeof(cl_mem), &buffers->mat_cross_section);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 6, sizeof(cl_mem), &buffers->velocity_delta);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 7, sizeof(cl_mem), &buffers->mu);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 8, sizeof(cl_mem), &buffers->dd_i);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 9, sizeof(cl_mem), &buffers->dd_j);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 10, sizeof(cl_mem), &buffers->dd_k);
+//    err |= clSetKernelArg(context->kernels.calc_denominator, 11, sizeof(cl_mem), &buffers->denominator);
+//    check_ocl(err, "Setting denominator kernel arguments");
+//   
+//    //size_t global[] = {problem->nang, problem->ng};
+//    size_t global[3] = {problem->nang, problem->ng,1};
+//    err = clEnqueueNDRangeKernel(context->queue,
+//        context->kernels.calc_denominator,
+//        2, 0, global, NULL,
+//        0, NULL, &denominator_event);
    
-    //size_t global[] = {problem->nang, problem->ng};
-    size_t global[3] = {problem->nang, problem->ng,1};
-    err = clEnqueueNDRangeKernel(context->queue,
-        context->kernels.calc_denominator,
-        2, 0, global, NULL,
-        0, NULL, &denominator_event);
-   */
     size_t global[3] = {problem->nang, problem->ng,1};
     size_t local[3] ={0,0,0};
     err= meta_gen_opencl_calc_denominator_calc_denominator(context->queue, global, local, rankinfo->nx, rankinfo->ny, rankinfo->nz, problem->nang, problem->ng, &buffers->mat_cross_section, &buffers->velocity_delta, &buffers->mu, &buffers->dd_i, &buffers->dd_j, &buffers->dd_k, &buffers->denominator, 0, &denominator_event);
