3c02162d13b10b14c9f9344581af7ef2acb36e38
Paul Sathre <sath6220@cs.vt.edu>
2020-02-01 09:20:09 -0800

    Fixes for MPI-recv zero_buffer and capping reduce work items at 128

--- src/scalar_flux.c
+++ src/scalar_flux.c
@@ -15,6 +15,7 @@ void compute_scalar_flux(
 
     // get closest power of 2 to nang
     size_t power = 1 << (unsigned int)ceil(log2((double)problem->nang));
+	if (power > 128) power = 128;
 
     const size_t global[] = {power * problem->ng, rankinfo->nx*rankinfo->ny*rankinfo->nz};
     const size_t local[] = {power, 1};
@@ -75,6 +76,7 @@ void compute_scalar_flux_moments(
 
     // get closest power of 2 to nang
     size_t power = 1 << (unsigned int)ceil(log2((double)problem->nang));
+	if (power > 128) power = 128;
 
     const size_t global[] = {power * problem->ng, rankinfo->nx*rankinfo->ny*rankinfo->nz};
     const size_t local[] = {power, 1};
